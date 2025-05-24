e884c78540f93bd80a8637c20b7217643957ade9
Jaeden <ch.schwartze@gmail.com>
2025-05-24 18:08:40 +0200

    chore: mark as 8.1 requirement at least

--- productbird.php
+++ productbird.php
@@ -6,7 +6,7 @@
  * Version:     1.1.0
  * Requires at least: 5.0
  * Tested up to: 6.8
- * Requires PHP: 7.4
+ * Requires PHP: 8.1
  * Author:      Productbird
  * License:     GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
